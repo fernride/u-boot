331db40e0089d7373901549d74dc12bcc0005e22
Alexandru Moise <alexandru.moise@fernride.com>
2024-01-15 09:40:36 +0100

    Add some fixes that allow sja1105 net driver to init
    This also depends on having the latest DTS changes from ATF that adds gmac0
    as an alias node in the DTS. Since sja1105 looks for the gmac0 node in the DTS
    by alias not by path

--- board/nxp/s32-cc/s32g/s32gxxxardb.c
+++ board/nxp/s32-cc/s32g/s32gxxxardb.c
@@ -12,7 +12,7 @@
 #define SARADC0_CH5		5
 #define SARADC0_TOLERANCE	200
 
-#define SPI5_ALIAS "spi5"
+#define SPI2_ALIAS "spi2"
 #define SJA1110_UC_COMPATIBLE "nxp,sja1110-uc"
 #define SJA1110_SWITCH_COMPATIBLE "nxp,sja1110-switch"
 #define SJA1110_DSA_COMPATIBLE "nxp,sja1110a"
@@ -243,53 +243,53 @@ static void compatible_err(const char *path, const char *compat, int err)
 
 static void ft_sja1110_dsa_setup(void *blob)
 {
-	const char *spi5_path;
-	int spi5_offset;
+	const char *spi2_path;
+	int spi2_offset;
 	int offset;
 	int ret;
 
 	if (env_get_yesno("sja1110_dsa") < 1)
 		return;
 
-	spi5_path = fdt_get_alias(blob, SPI5_ALIAS);
-	if (!spi5_path) {
-		pr_err("Alias not found: %s\n", SPI5_ALIAS);
+	spi2_path = fdt_get_alias(blob, SPI2_ALIAS);
+	if (!spi2_path) {
+		pr_err("Alias not found: %s\n", SPI2_ALIAS);
 		goto out_err;
 	}
 
-	spi5_offset = fdt_path_offset(blob, spi5_path);
-	if (spi5_offset < 0) {
+	spi2_offset = fdt_path_offset(blob, spi2_path);
+	if (spi2_offset < 0) {
 		pr_err("Failed to get node offset by path %s. Error: %s\n",
-		       spi5_path, fdt_strerror(spi5_offset));
+		       spi2_path, fdt_strerror(spi2_offset));
 		goto out_err;
 	}
 
-	offset = fdt_node_offset_by_compatible(blob, spi5_offset,
+	offset = fdt_node_offset_by_compatible(blob, spi2_offset,
 					       SJA1110_UC_COMPATIBLE);
 	if (offset >= 0)
 		fdt_del_node(blob, offset);
 	else
-		compatible_err(spi5_path, SJA1110_UC_COMPATIBLE, offset);
+		compatible_err(spi2_path, SJA1110_UC_COMPATIBLE, offset);
 
-	offset = fdt_node_offset_by_compatible(blob, spi5_offset,
+	offset = fdt_node_offset_by_compatible(blob, spi2_offset,
 					       SJA1110_SWITCH_COMPATIBLE);
 	if (offset >= 0)
 		fdt_del_node(blob, offset);
 	else
-		compatible_err(spi5_path, SJA1110_SWITCH_COMPATIBLE,
+		compatible_err(spi2_path, SJA1110_SWITCH_COMPATIBLE,
 			       offset);
 
-	offset = fdt_node_offset_by_compatible(blob, spi5_offset,
+	offset = fdt_node_offset_by_compatible(blob, spi2_offset,
 					       SJA1110_DSA_COMPATIBLE);
 	if (offset < 0) {
-		compatible_err(spi5_path, SJA1110_DSA_COMPATIBLE, offset);
+		compatible_err(spi2_path, SJA1110_DSA_COMPATIBLE, offset);
 		goto out_err;
 	}
 
 	ret = fdt_status_okay(blob, offset);
 	if (ret < 0) {
 		pr_err("Failed to set status \"okay\". Error: %s\n",
-		       fdt_strerror(spi5_offset));
+		       fdt_strerror(spi2_offset));
 		goto out_err;
 	}
 
